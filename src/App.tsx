4d4be138e29e186e4fde7c6d2ce28f1baf27ae1a
ruicai <cairui19990512>
2021-05-01 22:55:49 -0400

    Initial version of OMDb_Searcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+interface Movie {
+    title: string,
+    yearOfLease: number,
+    poster: string,
+    imdbID: string,
+}
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [movieTitle, setMovieTitle] = useState("");
+    const [movies, setMovies] = useState<Array<Movie>>([]);
+    const [nominations, setNominations] = useState<Array<Movie>>([]);
+
+    useEffect(() => {
+        if (nominations.length === 5) {
+            alert("well done")
+        }
+    }, [nominations]);
+
+    const onChangeMovieTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setMovieTitle(e.target.value);
+    }
+
+    const fetchMovieFromAPI = async (movieTitle: string) => {
+        const apiResponse = await fetch('https://www.omdbapi.com/?s=' + movieTitle + '&type=movie&apikey=9c01b986')
+        if (apiResponse.ok) {
+            const movieArray = await apiResponse.json()
+            console.log(movieArray);
+            const movieItems = movieArray["Search"];
+            console.log(movieItems);
+            const movArr = movieItems.map((mov: any) => ({
+                title: mov.Title,
+                yearOfLease: mov.Year,
+                poster: mov.Poster,
+                imdbID: mov.imdbID
+            }));
+            console.log(movArr)
+            setMovies(movArr)
+        }
+    }
+
+    const onClickSearchBtn = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+        await fetchMovieFromAPI(movieTitle);
+        console.log(movies)
+    }
+
+    const nominateMovie = (nominatedMovie: Movie) => {
+        if(nominations.length<5){
+            setNominations([...nominations, nominatedMovie])
+        }else {
+            alert("well done")
+        }
+    }
+
+    const deNominateMovie = (deNominatedMovie: Movie) => {
+        setNominations(nominations.filter((mov) => {
+            return mov.imdbID !== deNominatedMovie.imdbID;
+        }))
+    }
+
+    return (
+        <div className="App">
+            <form>
+                <div>
+                    <input onChange={onChangeMovieTitle} value={movieTitle}/>
+                </div>
+                <div>
+                    <button onClick={onClickSearchBtn}>Search</button>
+                </div>
+            </form>
+
+            <div>
+                {movies.map((mov, index) => (
+                    <div key={index}>
+                        <p>
+                            {mov.title} - Posted on <span>{mov.yearOfLease}</span>
+                            {nominations.length<5 && (
+                                <button onClick={() => nominateMovie(mov)} type={"button"}>
+                                    Nominate
+                                </button>
+                            )}
+                        </p>
+                    </div>
+                ))}
+            </div>
+
+            <div>
+                {nominations.map((mov, index) => (
+                    <div key={index}>
+                        <p>
+                            {mov.title} - Posted on <span>{mov.yearOfLease}</span>
+                        </p>
+                        <button onClick={() => deNominateMovie(mov)} type={"button"}>
+                            Remove
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/SearchResult/SearchMovieInput.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SearchMovieInput = () => {
+
+
+// return (
+//     <form>
+//         <div>
+//             <input onChange={onChangeMovieTitle} value={movieTitle}/>
+//         </div>
+//         <div>
+//             <button onClick={onClickSearchBtn}>Search</button>
+//         </div>
+//     </form>
+// )
+}
+
+export default SearchMovieInput
