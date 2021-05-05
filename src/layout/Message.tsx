7c5e9637d016c7363179082acd8cca461abf3c05
ruicai <cairui19990512>
2021-05-05 14:13:23 -0400

    Refactor Message.tsx by using useEffect hook

--- src/layout/Message.tsx
+++ src/layout/Message.tsx
@@ -1,6 +1,6 @@
 import {useSelector} from "react-redux";
 import {AppState} from "../store/AppState";
-import React from "react";
+import {useEffect} from "react";
 import {messageObj} from "../actions/MessageAction";
 import {useSnackbar} from 'notistack';
 
@@ -8,18 +8,20 @@ const Message = () => {
     const _messages = useSelector((state: AppState) => state.messages);
     const messages: Array<messageObj> = _messages ? _messages['messageArray'] : [];
     const {enqueueSnackbar} = useSnackbar();
-    const showMsg = (message: messageObj) => {
-        enqueueSnackbar(message.message, {variant: message.alertType, preventDuplicate: true})
-    }
 
-    return (
-        <>
-            {messages !== null && messages.length > 0 &&
-            messages.map((item: messageObj, index) => (
-                showMsg(item)))
-            }
-        </>
-    )
+
+    useEffect(() => {
+        const showMsg = (message: messageObj) => {
+            enqueueSnackbar(message.message, {
+                variant: message.alertType,
+                preventDuplicate: true,
+            })
+        }
+        messages !== null && messages.length > 0 &&
+        messages.map((item: messageObj, index) => (showMsg(item)))
+    }, [messages, enqueueSnackbar])
+
+    return null
 
 }
 
